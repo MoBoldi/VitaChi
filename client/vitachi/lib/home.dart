92d092d92645391901f2e1c6208dd6920abeda9b
Moritz Weibold <moritz@weibold.cc>
2020-12-11 10:11:45 +0100

    added routes to the app

---
+++ client/vitachi/lib/home.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("VitaChi"),
+        centerTitle: true,
+        backgroundColor: Colors.purpleAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.white),
+            onPressed: null,
+          )
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              Text("this is VitaChi"),
+              Text("next step is a slider"),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/logo.png'),
+                    )
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(),
+            ),
+            ListTile(
+              title: Text(
+                "Home",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+            ListTile(
+              title: Text(
+                "Eingaben",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text(
+                "Statistiken",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+                Navigator.pushNamed(context, '/statistics');
+              },
+            ),
+            ListTile(
+              title: Text(
+                "Freunde",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text(
+                "Shop",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text(
+                "ToDo",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
