db53819436ddf07686f19123827dcfceedfd683c
NoahStallinger <noahstalli11@gmail.com>
2021-01-27 18:24:31 +0100

    Menü Eingaben

--- client/vitachi/lib/pages/EssenNav.dart
+++ client/vitachi/lib/pages/EssenNav.dart
@@ -11,6 +11,8 @@ class EingabenNav extends StatefulWidget {
 class _EingabenNavState extends State<EingabenNav> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final Color color = Color(0xffbbedc5);
     return Scaffold(
       appBar: MyAppBar(context, 'VitaChi', null),
       drawer: MyDrawer(),
@@ -18,194 +20,234 @@ class _EingabenNavState extends State<EingabenNav> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Container(
-              height: 120,
-              margin: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ]),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image(
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/essen');
+              },
+              child: Container(
+                height: size.height/5.5,
+                margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Image(
+                        height: size.height/8,
                         image: AssetImage('assets/salad.png'),
                       ),
+                    SizedBox(
+                      width: size.width/10,
                     ),
-                    flex: 2,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                              child: Text('Essen', style: TextStyle(fontSize: size.width/17,fontWeight: FontWeight.bold),
+                              )
+                          ),
+                        ],
+                    ),
+                    SizedBox(
+                      width: size.width/6,
+                    ),
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text('Essen'),
-                        Text('Hier eingeben zum Essen'),
-                        FlatButton(
+                        Container(
+                            child: Icon(
+                              Icons.arrow_forward_outlined,
+                              ),
+                          width: size.width/4,
                           color: Color(0xffbbedc5),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/essen');
-                          },
-                          child: Text('Hinzufügen'),
+                          height: size.height/5.5,
                         )
                       ],
                     ),
-                    flex: 6,
-                  )
-                ],
+                  ],
+                ),
               ),
             ),
-            Container(
-              height: 120,
-              margin: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ]),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image(
-                        image: AssetImage('assets/basketball-ball.png'),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/bewegung');
+              },
+              child: Container(
+                height: size.height/5.5,
+                margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
                       ),
+                    ]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Image(
+                      height: size.height/8,
+                      image: AssetImage('assets/basketball-ball.png'),
                     ),
-                    flex: 2,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    SizedBox(
+                      width: size.width/13,
+                    ),
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text('Bewegung'),
-                        Text('Bewerte hier deine letzte aktive Bewegung'),
-                        FlatButton(
+                        Container(
+                            child: Text('Bewegung', style: TextStyle(fontSize: size.width/17,fontWeight: FontWeight.bold),
+                            )
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: size.width/13,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: Icon(
+                            Icons.arrow_forward_outlined,
+                          ),
+                          width: size.width/4,
                           color: Color(0xffbbedc5),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/bewegung');
-                          },
-                          child: Text('Hinzufügen'),
+                          height: size.height/5.5,
                         )
                       ],
                     ),
-                    flex: 6,
-                  )
-                ],
+                  ],
+                ),
               ),
             ),
-            Container(
-              height: 120,
-              margin: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ]),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image(
-                        image: AssetImage('assets/night.png'),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/schlafen');
+              },
+              child: Container(
+                height: size.height/5.5,
+                margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
                       ),
+                    ]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Image(
+                      height: size.height/8,
+                      image: AssetImage('assets/night.png'),
                     ),
-                    flex: 2,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    SizedBox(
+                      width: size.width/10,
+                    ),
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text('Schlaf'),
-                        Text('Bewerte hier deinen letzten Schlaf'),
-                        FlatButton(
+                        Container(
+                            child: Text('Schlaf', style: TextStyle(fontSize: size.width/17,fontWeight: FontWeight.bold),
+                            )
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: size.width/6,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: Icon(
+                            Icons.arrow_forward_outlined,
+                          ),
+                          width: size.width/4,
                           color: Color(0xffbbedc5),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/schlafen');
-                          },
-                          child: Text('Hinzufügen'),
+                          height: size.height/5.5,
                         )
                       ],
                     ),
-                    flex: 6,
-                  )
-                ],
+                  ],
+                ),
               ),
             ),
-            Container(
-              height: 120,
-              margin: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ]),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image(
-                        image: AssetImage('assets/laptop.png'),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/arbeit');
+              },
+              child: Container(
+                height: size.height/5.5,
+                margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
                       ),
+                    ]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Image(
+                      height: size.height/8,
+                      image: AssetImage('assets/laptop.png'),
                     ),
-                    flex: 2,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    SizedBox(
+                      width: size.width/10,
+                    ),
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text('Arbeit'),
-                        Text('Hier kannst du deine Arbeitszeit eingeben'),
-                        FlatButton(
+                        Container(
+                            child: Text('Arbeit', style: TextStyle(fontSize: size.width/17,fontWeight: FontWeight.bold),
+                            )
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: size.width/6,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: Icon(
+                            Icons.arrow_forward_outlined,
+                          ),
+                          width: size.width/4,
                           color: Color(0xffbbedc5),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/arbeit');
-                          },
-                          child: Text('Hinzufügen'),
+                          height: size.height/5.5,
                         )
                       ],
                     ),
-                    flex: 6,
-                  )
-                ],
+                  ],
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
