264b866af0420d9480fe739d77fa9cb043806eb8
stephanherbe <herbe.stephan@gmail.com>
2021-03-23 12:42:12 +0100

    Wohnzimmer

--- client/vitachi/lib/pages/ShopBody.dart
+++ client/vitachi/lib/pages/ShopBody.dart
@@ -9,19 +9,17 @@ import 'package:vitachi/pages/detail_shop.dart';
 class Body extends StatefulWidget {
   @override
   _BodyState createState() => _BodyState();
-
 }
 
-
 class _BodyState extends State<Body> {
-
   var products = List<Produkt>();
 
   Future<List<Produkt>> getData() async {
-    Response response = await get('http://10.0.2.2:8080/vitaChi/findAll/Accessoire');
+    Response response =
+        await get('http://10.0.2.2:8080/vitaChi/findAll/Accessoire');
     print("response" + response.body);
     var productsJson = json.decode(response.body);
-    for(var productJson in productsJson){
+    for (var productJson in productsJson) {
       products.add(Produkt.fromJson(productJson));
     }
     print(products[0].bildpfad);
@@ -49,68 +47,74 @@ class _BodyState extends State<Body> {
                   .copyWith(fontWeight: FontWeight.bold),
             ),
           ),
-
           Expanded(
               child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: FutureBuilder(
-              future: getData(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return GridView.builder(
-                      itemCount: products.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 20,
-                          crossAxisSpacing: 20,
-                          childAspectRatio: 0.75),
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(
-                    builder: (context) =>
-                        DetailShop(data: products[index],)));
-                          },
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                  child: Container(
-                                      padding: EdgeInsets.all(20),
-                                      decoration: BoxDecoration(
-                                          gradient: LinearGradient(
-                                              begin: Alignment.bottomLeft,
-                                              end: Alignment.topRight,
-                                              colors: [Color(0xff54a2fc), Color(0xff6bc2fa)]),
-                                          borderRadius: BorderRadius.circular(
-                                              16)),
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                        children: [
-                                          Image(
-                                            image: AssetImage(products[index].bildpfad)
-                                          ),
-                                          Text(
-                                            products[index].bezeichnung,
-                                            style: TextStyle(
-                                                fontSize: MediaQuery.of(context).size.width/20,
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold
+                future: getData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return GridView.builder(
+                        itemCount: products.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 20,
+                            crossAxisSpacing: 20,
+                            childAspectRatio: 0.75),
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => DetailShop(
+                                            data: products[index],
+                                          )));
+                            },
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                    child: Container(
+                                        padding: EdgeInsets.all(20),
+                                        decoration: BoxDecoration(
+                                            gradient: LinearGradient(
+                                                begin: Alignment.bottomLeft,
+                                                end: Alignment.topRight,
+                                                colors: [
+                                                  Color(0xff54a2fc),
+                                                  Color(0xff6bc2fa)
+                                                ]),
+                                            borderRadius:
+                                                BorderRadius.circular(16)),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            Image(
+                                                image: AssetImage(
+                                                    products[index].bildpfad)),
+                                            Text(
+                                              products[index].bezeichnung,
+                                              style: TextStyle(
+                                                  fontSize:
+                                                      MediaQuery.of(context)
+                                                              .size
+                                                              .width /
+                                                          20,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold),
                                             ),
-                                          ),
-                                        ],
-                                      )
-                                  )
-                              ),
-                            ],
-                          ),
-                        );
-                      });
-                }else{
-                  return Image(image: AssetImage('assets/LogoSlider.png'));
-                }
-              }
-            ),
+                                          ],
+                                        ))),
+                              ],
+                            ),
+                          );
+                        });
+                  } else {
+                    return Image(image: AssetImage('assets/LogoSlider.png'));
+                  }
+                }),
           ))
         ],
       ),
@@ -120,7 +124,7 @@ class _BodyState extends State<Body> {
 
 class ProductsList extends StatelessWidget {
   final List<Produkt> products;
-  ProductsList({Key key, this.products}) : super (key: key);
+  ProductsList({Key key, this.products}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -149,33 +153,26 @@ class ProductsList extends StatelessWidget {
                                 begin: Alignment.bottomLeft,
                                 end: Alignment.topRight,
                                 colors: [Color(0xff54a2fc), Color(0xff6bc2fa)]),
-                            borderRadius: BorderRadius.circular(
-                                16)),
+                            borderRadius: BorderRadius.circular(16)),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             Image(
-                              image: NetworkImage(
-                                  products[index].bildpfad),
+                              image: NetworkImage(products[index].bildpfad),
                             ),
                             Text(
                               products[index].bezeichnung,
                               style: TextStyle(
-                                  fontSize: MediaQuery.of(context).size.width/20,
+                                  fontSize:
+                                      MediaQuery.of(context).size.width / 20,
                                   color: Colors.white,
-                                  fontWeight: FontWeight.bold
-                              ),
+                                  fontWeight: FontWeight.bold),
                             ),
                           ],
-                        )
-                    )
-                ),
+                        ))),
               ],
             ),
           );
         });
   }
 }
-
-
-
--- client/vitachi/lib/pages/Wohnzimmer.dart
+++ client/vitachi/lib/pages/Wohnzimmer.dart
@@ -1,10 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:vitachi/components/myAppBar.dart';
 import 'package:vitachi/components/myAppBarEingaben.dart';
 import 'package:vitachi/components/myDrawer.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:vitachi/entitys/Produkt.dart';
+import 'package:vitachi/pages/home.dart';
+import 'package:shape_of_view/shape/arc.dart';
+import 'package:shape_of_view/shape_of_view.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class Wohnzimmer extends StatefulWidget {
+  final List<ChartData> ges = [
+    ChartData('Wellbeing', 3, Color(0xFF4DA8DA)),
+    ChartData('', 2, Color(0xFF9dc6dd)),
+  ];
+  List<ChartData> food = [
+    ChartData('Food', 5, Color(0xFF4DA8DA)),
+    ChartData('', 1, Color(0xFF9dc6dd)),
+  ];
+  final List<ChartData> movement = [
+    ChartData('Movement', 3, Color(0xFF4DA8DA)),
+    ChartData('', 2, Color(0xFF9dc6dd)),
+  ];
+  final List<ChartData> sleep = [
+    ChartData('Sleep', 2, Color(0xFF4DA8DA)),
+    ChartData('', 3, Color(0xFF9dc6dd)),
+  ];
+  final List<ChartData> work = [
+    ChartData('Work', 30, Color(0xFF4DA8DA)),
+    ChartData('', 20, Color(0xFF9dc6dd)),
+  ];
+
+  Map data = {};
+
   @override
   _WohnzimmerState createState() => _WohnzimmerState();
 }
@@ -14,21 +45,75 @@ class _WohnzimmerState extends State<Wohnzimmer> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    var products = List<Produkt>();
+
+    Future<List<Produkt>> getData() async {
+      Response response =
+          await get('http://10.0.2.2:8080/vitaChi/findAll/Accessoire');
+      print("response" + response.body);
+      var productsJson = json.decode(response.body);
+      for (var productJson in productsJson) {
+        products.add(Produkt.fromJson(productJson));
+      }
+      print(products[0].bildpfad);
+    }
+
+    /*data = ModalRoute.of(context).settings.arguments;
+    if (data == null) {
+      print('It is null');
+      data = {
+        'arg': food[0].y,
+      };
+    } else {
+      print('It is not null');
+      food[0].y = data['avg'];
+      food[1].y = 5 - data['avg'];
+    }
+    print(data);*/
 
     return Scaffold(
         appBar: MyAppBarWohnzimmer(context, 'VitaChi', null),
         drawer: MyDrawer(),
         body: Stack(
           children: [
-            Container(
+            DragTarget<String>(
+              builder: (
+                BuildContext context,
+                data,
+                rejected,
+              ) {
+                return Container(
+                  child:
+                      Image(image: AssetImage("assets/Blume_Placeholder.png")),
+                  height: height * 0.8,
+                  width: width,
+                  margin: EdgeInsets.only(top: height * 0.2),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/boden.jpg"),
+                          fit: BoxFit.fill)),
+                );
+              },
+              onAccept: (String data) {
+                setState(() {
+                  return Image.asset('assets/test2.png');
+                });
+              },
+            ),
+
+            /* Container(
+              /*child: Image(
+                width: MediaQuery.of(context).size.width * 0.5,
+                image: getEmotion(food[0].y),
+              ),*/
               child: Image(image: AssetImage("assets/Blume_Placeholder.png")),
-              height: height,
+              height: height * 0.8,
+              width: width,
+              margin: EdgeInsets.only(top: height * 0.2),
               decoration: BoxDecoration(
                   image: DecorationImage(
-                image: AssetImage("assets/floor.png"),
-                fit: BoxFit.cover,
-              )),
-            ),
+                      image: AssetImage("assets/boden.jpg"), fit: BoxFit.fill)),
+            ),*/
             Stack(
               children: [
                 Container(
@@ -39,7 +124,7 @@ class _WohnzimmerState extends State<Wohnzimmer> {
                 DraggableScrollableSheet(
                   initialChildSize: 0.1,
                   minChildSize: 0.1,
-                  maxChildSize: 0.6,
+                  maxChildSize: 0.3,
                   builder: (BuildContext context, myScrollController) {
                     return Container(
                       child: ListView(
@@ -49,6 +134,24 @@ class _WohnzimmerState extends State<Wohnzimmer> {
                             height: 200.0,
                             width: width,
                             color: Colors.red,
+                            child: Container(
+                                alignment: Alignment.center,
+                                width: width * 0.3,
+                                height: 100.0,
+                                color: Colors.white,
+                                child: Draggable<String>(
+                                  data: 'Item',
+                                  child: Image.asset(
+                                    'assets/test2.png',
+                                    width: width * 0.3,
+                                    height: 100.0,
+                                  ),
+                                  feedback: Image.asset(
+                                    'assets/test2.png',
+                                    width: width,
+                                    height: height,
+                                  ),
+                                )),
                           ),
                           Container(
                             height: 200.0,
@@ -74,5 +177,21 @@ class _WohnzimmerState extends State<Wohnzimmer> {
             ),
           ],
         ));
+  }
+}
+
+AssetImage getEmotion(double wellbeing) {
+  if (1 <= wellbeing && wellbeing < 2) {
+    return AssetImage('assets/Blume_1.png');
+  } else if (2 <= wellbeing && wellbeing < 3) {
+    return AssetImage('assets/Blume_2.png');
+  } else if (3 <= wellbeing && wellbeing < 4) {
+    return AssetImage('assets/Blume_3.png');
+  } else if (4 <= wellbeing && wellbeing < 4.5) {
+    return AssetImage('assets/Blume_4.png');
+  } else if (4.5 <= wellbeing && wellbeing <= 5) {
+    return AssetImage('assets/Blume_5.png');
+  } else {
+    return AssetImage('assets/Blume_3.png');
   }
 }
