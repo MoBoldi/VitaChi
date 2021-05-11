3cbc91773630377024073e503638de0c7f42e998
Markus Stadler <markus.stadler4092@gmail.com>
2021-05-11 23:24:31 +0200

    Registrierung per Keycloak Oberfläche

--- client/vitachi/lib/pages/Register.dart
+++ client/vitachi/lib/pages/Register.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vitachi/pages/TextFieldWidget.dart';
 import 'package:vitachi/pages/WaveWidget.dart';
 import 'package:http/http.dart' as http;
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class Register extends StatefulWidget {
@@ -15,139 +18,20 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final eingaben = GlobalKey<FormState>();
   final Color color = Color(0xff3f8ee9);
+
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: [Color(0xff3f8ee9), Color(0xff5db4ec)]),
-                    ),
-                    height: MediaQuery.of(context).size.height/2,
-                  ),
-                  AnimatedPositioned(
-                    duration: Duration(milliseconds: 20000),
-                    child: WaveWidget(
-                      size: size,
-                      yOffset: size.height / 3,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Center(
-                    child: Container(
-                      height: MediaQuery.of(context).size.height/3.5,
-
-                      margin: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height/25, 0, 0),
-                      child: Image(
-                        image: AssetImage('assets/Logo_Design_03.png'),
-                        width: MediaQuery.of(context).size.width/1.5,
-                      ),
-                    ),
-                  ),
-                  Container(
-                  margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/2.5),
-                    child: Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(top: size.height/40),
-                          child:
-                          Form(
-                            key: eingaben,
-                            child: Column(
-                              children: [
-                                TextFieldWidget(
-                                  hintText: 'Benutzername',
-                                  prefixIconData: Icons.person_outline,
-                                  obscureText: false,
-                                ),
-                                Container(
-                                  height: MediaQuery.of(context).size.height/32,
-                                ),
-                                TextFieldWidget(
-                                  hintText: 'EMail',
-                                  prefixIconData: Icons.mail_outline,
-                                  obscureText: false,
-                                ),
-                                Container(
-                                  height: MediaQuery.of(context).size.height/32,
-                                ),
-                                TextFieldWidget(
-                                  hintText: 'Passwort',
-                                  prefixIconData: Icons.lock_outline,
-                                  obscureText: true,
-                                ),
-                                Container(
-                                  height: MediaQuery.of(context).size.height/15,
-                                ),
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width/1.2,
-                                  height: MediaQuery.of(context).size.height/15,
-                                  child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          primary: Color(0xff3f8ee9),
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(50)
-                                          )
-                                      ),
-
-                                      onPressed: () {
-                                        registerNewUser();
-                                        if (eingaben.currentState.validate()) {
-                                          // Process data.
-                                          Navigator.pushNamed(context, '/');
-                                        }
-                                      },
-                                      child: Text('Registrieren', style: TextStyle(color: Colors.white))
-                                  ),
-                                ),
-                                Container(
-                                  height: MediaQuery.of(context).size.height/70,
-                                ),
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width/1.2,
-                                  height: MediaQuery.of(context).size.height/15,
-                                  child: FlatButton(
-                                    onPressed: (){
-                                      Navigator.pushReplacementNamed(context, '/login');
-                                    },
-                                    child: Text("Zurück", style: TextStyle(color: Colors.black)),
-                                    shape: RoundedRectangleBorder(side: BorderSide(
-                                        color: Color(0xff3f8ee9),
-                                        width: 1,
-                                        style: BorderStyle.solid
-                                    ),
-                                        borderRadius: BorderRadius.circular(50)
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-
-                          ),
-                          color: Colors.white,
-                        )
-                      ],
-                    ),
-                    color: Colors.white,
-                  ),
-                ]
-            ),
-
-
-          ],
-        ),
+      body: WebView(
+        initialUrl: 'http://10.0.2.2:8010/auth/realms/vitachi/protocol/openid-connect/registrations?client_id=account&redirect_uri=http%3A%2F%2F10.0.2.2%3A8010%2Fauth%2Frealms%2Fvitachi%2Faccount%2Flogin-redirect&state=0%2F14997994-c8da-4f2a-b57e-c0e91d48bc3e&response_type=code&scope=openid',
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
       ),
-
-
     );
   }
 }
