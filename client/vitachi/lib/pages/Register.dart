cf51f35476a3e7a82fbe713685ed5b5d4e72273f
Markus Stadler <markus.stadler4092@gmail.com>
2021-05-12 10:29:33 +0200

    Flutter WebView eingefügt

--- client/vitachi/lib/pages/Register.dart
+++ client/vitachi/lib/pages/Register.dart
@@ -6,8 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:vitachi/pages/TextFieldWidget.dart';
 import 'package:vitachi/pages/WaveWidget.dart';
 import 'package:http/http.dart' as http;
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 
 class Register extends StatefulWidget {
@@ -16,28 +15,140 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  final eingaben = GlobalKey<FormState>();
-  final Color color = Color(0xff3f8ee9);
 
-  Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Color color = Color(0xff3f8ee9);
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+  StreamSubscription<String> _onUrlChanged;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterWebViewPlugin.close();
+
+    _onUrlChanged = flutterWebViewPlugin.onUrlChanged.listen((String url) {
+      if (mounted) {
+        setState(() {
+          flutterWebViewPlugin.close();
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _onUrlChanged.cancel();
+
+    flutterWebViewPlugin.dispose();
+
+    super.dispose();
+  }
+
+  String selectedUrl = 'http://10.0.2.2:8010/auth/realms/vitachi/protocol/openid-connect/registrations?client_id=account&redirect_uri=http%3A%2F%2F10.0.2.2%3A8010%2Fauth%2Frealms%2Fvitachi%2Faccount%2Flogin-redirect&state=0%2F14997994-c8da-4f2a-b57e-c0e91d48bc3e&response_type=code&scope=openid';
+
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: WebView(
-        initialUrl: 'http://10.0.2.2:8010/auth/realms/vitachi/protocol/openid-connect/registrations?client_id=account&redirect_uri=http%3A%2F%2F10.0.2.2%3A8010%2Fauth%2Frealms%2Fvitachi%2Faccount%2Flogin-redirect&state=0%2F14997994-c8da-4f2a-b57e-c0e91d48bc3e&response_type=code&scope=openid',
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-        },
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(controller: _urlCtrl),
+            ),
+            RaisedButton(
+              onPressed: () {
+                flutterWebViewPlugin.launch(
+                  selectedUrl,
+                  rect: Rect.fromLTWH(
+                      0.0, 0.0, MediaQuery.of(context).size.width, 300.0),
+                  userAgent: kAndroidUserAgent,
+                  invalidUrlRegex:
+                  r'^(https).+(twitter)', // prevent redirecting to twitter when user click on its icon in flutter website
+                );
+              },
+              child: const Text('Open Webview (rect)'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                flutterWebViewPlugin.launch(selectedUrl, hidden: true);
+              },
+              child: const Text('Open "hidden" Webview'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                flutterWebViewPlugin.launch(selectedUrl);
+              },
+              child: const Text('Open Fullscreen Webview'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/widget');
+              },
+              child: const Text('Open widget webview'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(controller: _codeCtrl),
+            ),
+            RaisedButton(
+              onPressed: () {
+                final future =
+                flutterWebViewPlugin.evalJavascript(_codeCtrl.text);
+                future.then((String result) {
+                  setState(() {
+                    _history.add('eval: $result');
+                  });
+                });
+              },
+              child: const Text('Eval some javascript'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                final future = flutterWebViewPlugin.evalJavascript('alert("Hello World");');
+                future.then((String result) {
+                  setState(() {
+                    _history.add('eval: $result');
+                  });
+                });
+              },
+              child: const Text('Eval javascript alert()'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _history.clear();
+                });
+                flutterWebViewPlugin.close();
+              },
+              child: const Text('Close'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                flutterWebViewPlugin.getCookies().then((m) {
+                  setState(() {
+                    _history.add('cookies: $m');
+                  });
+                });
+              },
+              child: const Text('Cookies'),
+            ),
+            Text(_history.join('\n'))
+          ],
+        ),
       ),
     );
   }
 }
 
 Future<void> registerNewUser() async {
-   Future<http.Response> r = http.post(
+  Future<http.Response> r = http.post(
     Uri.http('10.0.2.2:8080', 'vitaChi/registerNewUser'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
@@ -51,5 +162,5 @@ Future<void> registerNewUser() async {
     }),
   );
 
-   print(r.runtimeType);
+  print(r.runtimeType);
 }
