b6b0bdc8872f8b91877150bd6666872e62694b38
stephanherbe <herbe.stephan@gmail.com>
2021-06-05 18:23:31 +0200

    Foto 1 geändert

--- client/vitachi/lib/entitys/Produkt.dart
+++ client/vitachi/lib/entitys/Produkt.dart
@@ -1,24 +1,21 @@
-class Produkt{
+class Produkt {
   String beschreibung, bezeichnung, details, bildpfad;
   int preis, id;
-  Produkt({
-    this.id,
-    this.beschreibung,
-    this.bezeichnung,
-    this.details,
-    this.bildpfad,
-    this.preis
-});
-
+  Produkt(
+      {this.id,
+      this.beschreibung,
+      this.bezeichnung,
+      this.details,
+      this.bildpfad,
+      this.preis});
 
   factory Produkt.fromJson(Map<String, dynamic> json) {
     return Produkt(
-      id: json["accessoireID"],
-      bezeichnung: json["bezeichnung"],
-      beschreibung: json["beschreibung"],
-      preis: json["preis"],
-      details: json["details"],
-      bildpfad: json["bild_pfad"]
-    );
+        id: json["accessoireID"],
+        bezeichnung: json["bezeichnung"],
+        beschreibung: json["beschreibung"],
+        preis: json["preis"],
+        details: json["details"],
+        bildpfad: json["bild_pfad"]);
   }
 }
